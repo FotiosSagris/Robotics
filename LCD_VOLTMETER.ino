534bde9b568b10ec1a28f47c01b9c40aa97cff14
FotiosSagris <2578751@edu.vaniercollege.qc.ca>
2025-11-22 14:40:13 -0500

    Update LCD_VOLTMETER.ino

--- LCD_VOLTMETER.ino
+++ LCD_VOLTMETER.ino
@@ -1,14 +1,11 @@
-#include <LiquidCrystal.h>
+#include <LiquidCrystal.h> // including the LCD Display library
 
 // LCD pin setup
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 void setup() {
-  lcd.begin(16, 2);
-  lcd.print("Analog Reader");
-  delay(1000);
-  lcd.clear();
+  lcd.begin(16, 2); // Initialize the LCD with 16 columns and 2 rows
 }
 
 void loop() {
@@ -19,15 +16,17 @@ void loop() {
   float voltage = sensorValue * (5.0 / 1023.0);
 
   // LCD Display
-  lcd.setCursor(0, 0);
+  lcd.setCursor(0, 0); // set cursor to column 0, row 0
+  // Display the bitvalue
   lcd.print("Bit: ");
   lcd.print(sensorValue);
   lcd.print("    ");
 
-  lcd.setCursor(0, 1);
-  lcd.print("Volt: ");
+  lcd.setCursor(0, 1); // set cursor to column 0, row 1
+  // Display the voltage
+  lcd.print("Volt: "); 
   lcd.print(voltage, 2);
   lcd.print("V   ");
 
-  delay(200);
+  delay(200); // small delay
 }
